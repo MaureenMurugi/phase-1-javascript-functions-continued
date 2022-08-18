44d5a389093cf78a032c8e37fa36650eefdb23ba
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-18 11:18:02 +0300

    commit tree 579928e55979668590db4aafe92af0271f691337

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return (`This Saturday, I want to ${fun}!`)
+}
+saturdayFun();
+//saturdayFun("bathe my dog");
+
+let mondayWork = function(work = "go to the office") {
+    return (`This Monday, I will ${work}.`)
+}
+mondayWork();
+//mondayWork("work from home");
+
+function wrapAdjective(result = "*") {
+    return function(emphatic = "a hard worker"){
+        return `You are ${result}${emphatic}${result}!`;
+    }
+}
+wrapAdjective()("awesome");
+wrapAdjective("||")("a dedicated programmer!");
